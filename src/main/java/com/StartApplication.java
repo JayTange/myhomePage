d367c8e5dd3a0ac459cc4c8fb039a397a222131e
tangj <840391701@qq.com>
2018-09-10 10:32:25 +0800

    成功启动

--- src/main/java/com/StartApplication.java
+++ src/main/java/com/StartApplication.java
@@ -1,9 +1,16 @@
 package com;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author tangj
  * @date 2018/9/9 23:35
  */
+@SpringBootApplication
 public class StartApplication {
 
+    public static void main(String args[]){
+        new SpringApplication(StartApplication.class).run(args);
+    }
 }
